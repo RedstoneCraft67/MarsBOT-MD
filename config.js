05e0dd6d7cd7e9707dc61b9caa0b0834b69dd720
Klebitz12890 <97742308+Klebitz12890@users.noreply.github.com>
2022-02-13 07:21:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6283832492541'] // Put your number here
+global.owner = ['6281211006838'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -25,7 +25,7 @@ global.APIKeys = { // APIKey Here
   'https://melcanz.com': 'apikeylu',
   'https://api.lolhuman.xyz': 'apikeylu',
   'https://zahirr-web.herokuapp.com': 'apikeylu',
-  'https://api.zeks.xyz': 'apikeylu',
+  'https://api.zeks.me': 'apikeylu',
   'https://pencarikode.xyz': 'apikeylu',
   'https://leyscoders-api.herokuapp.com': 'apikeylu'
 }
@@ -34,15 +34,15 @@ global.APIKeys = { // APIKey Here
 global.packname = 'Made by'
 global.author = 'MarsBOT-MD'
 
-global.benar = 'benar mendapatkan'
-global.salah = 'salah'
-global.dikit = 'hampir benar'
+global.benar = 'Right'
+global.salah = 'Wrong'
+global.dikit = 'Almost Right'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = 'Made by : RHns'
+global.wm = 'Made by : Klebitz'
 global.instagram = 'https://instagram.com/bgsrhnsh'
 global.media = 'https://telegra.ph/file/1a0e237dc563e7cc1a143.jpg'
-global.wait = '*tunggu sedang diproses...*'
-global.eror = '*bermasalah...*'
+global.wait = '*Wait Still Processing...*'
+global.eror = '*Error...*'
 
 global.multiplier = 69 // The higher, The harder levelup
 
